2f5d912fe9cc25db9d30e12ddff970a8057d5b6c
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-03-21 10:37:29 +0600

    mongodb connection added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const repoRoutes = require('./routes/repoRoute');
 
 // app
@@ -19,7 +20,15 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/repos', repoRoutes);
 
-// listen for request
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+// mongodb
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for request
+    app.listen(port, () => {
+      console.log(`connected to mongodb and listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
