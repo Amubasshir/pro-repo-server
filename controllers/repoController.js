133e9ad32194c4f243e5e69bd5ac43b6443619f0
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-03-25 16:45:44 +0600

    update empty field added

--- controllers/repoController.js
+++ controllers/repoController.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Repo = require('../models/repoModel');
 
 // get all repo
-getAllRepos = async (req, res) => {
+const getAllRepos = async (req, res) => {
   const repos = await Repo.find({}).sort({ createdAt: -1 });
 
   res.status(200).json(repos);
@@ -48,13 +48,13 @@ const postRepo = async (req, res) => {
     emptyFields.push('commit');
   }
   if (!pr) {
-    emptyFields.push('pr ');
+    emptyFields.push('pr');
   }
 
   if (emptyFields.length > 0) {
     return res
-      .statue(400)
-      .json({ error: 'please fill in al fields', emptyFields });
+      .status(400)
+      .json({ error: 'Please fill in all fields', emptyFields });
   }
 
   try {
@@ -63,8 +63,8 @@ const postRepo = async (req, res) => {
     });
 
     res.status(200).json(repo);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
 };
 
@@ -85,10 +85,46 @@ const deleteRepo = async (req, res) => {
 // update a repo
 const updateRepo = async (req, res) => {
   const { id } = req.params;
+  const { title, subtitle, visibility, language, star, commit, pr } = req.body;
+
+  let emptyFields = [];
+  if (!title) {
+    emptyFields.push('title');
+  }
+  if (!subtitle) {
+    emptyFields.push('subtitle');
+  }
+  if (!visibility) {
+    emptyFields.push('visibility');
+  }
+  if (!language) {
+    emptyFields.push('language');
+  }
+
+  if (!star) {
+    emptyFields.push('star');
+  }
+  if (!commit) {
+    emptyFields.push('commit');
+  }
+  if (!pr) {
+    emptyFields.push('pr');
+  }
+
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: 'Please fill in all fields', emptyFields });
+  }
+
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: 'invalid id' });
   }
-  const repo = await Repo.findOneAndUpdate({ _id: id }, { ...req.body });
+  const repo = await Repo.findOneAndUpdate(
+    { _id: id },
+    { ...req.body },
+    { new: true }
+  );
   if (!repo) {
     return res.status(404).json({ error: 'NO project found' });
   }
