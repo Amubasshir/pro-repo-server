b626c46a7d68affcf329e47675718889520d74eb
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-03-26 08:20:26 +0600

    signup done and bcrypt added

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const schema = mongoose.Schema;
 
@@ -13,5 +14,22 @@ const userSchema = new schema({
     require: true,
   },
 });
+
+userSchema.statics.signup = async function (email, password) {
+  const exist = await this.findOne({ email });
+
+  if (exist) {
+    throw Error('Email is already used.');
+  }
+
+  // encrypt password
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  // create a user
+  const user = await this.create({ email: email, password: hash });
+
+  return user;
+};
 
 module.exports = mongoose.model('User', userSchema);
