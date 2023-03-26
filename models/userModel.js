9ef0e264414f354fd55e02bbf8df3722ec74a4c7
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-03-26 10:43:43 +0600

    validator added

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const schema = mongoose.Schema;
 
@@ -16,6 +17,25 @@ const userSchema = new schema({
 });
 
 userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw new Error('Please fill up all.');
+  }
+
+  // check email is valid
+  if (!validator.isEmail(email)) {
+    throw new Error(
+      'Error: The email address you entered is invalid. Please check the format of your email address and try again.'
+    );
+  }
+
+  // password must be lowercase, uppercase, number, symbol and 8+ chars
+  if (!validator.isStrongPassword(password)) {
+    throw new Error(
+      'Error: Your password must be at least 8 characters long and include a combination of uppercase and lowercase letters, numbers, and symbols. Please choose a stronger password to protect your account.'
+    );
+  }
+
   const exist = await this.findOne({ email });
 
   if (exist) {
